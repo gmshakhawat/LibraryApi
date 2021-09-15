8308fb231e18541c9ca1537f11be57395cda3409
shakhakhawat <shakhawatislam96@gmail.com>
2021-09-15 22:55:18 +0600

    Initial commit

---
+++ src/main/java/com/softrear/libraryapi/LibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.softrear.libraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApiApplication.class, args);
+    }
+
+}
